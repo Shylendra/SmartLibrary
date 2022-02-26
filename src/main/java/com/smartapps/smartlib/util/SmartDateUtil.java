d40b0eb2a6ac97e035a995f24c63535800bf1cc3
shylendra <shylendra.tm@gmail.com>
2022-02-26 18:40:38 +1100

    Initial push

--- src/main/java/com/smartapps/smartlib/util/SmartDateUtil.java
+++ src/main/java/com/smartapps/smartlib/util/SmartDateUtil.java
@@ -1,12 +1,43 @@
 package com.smartapps.smartlib.util;
 
+import java.sql.Date;
+import java.sql.Timestamp;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+
 import org.joda.time.LocalDateTime;
 
 public class SmartDateUtil {
 
-	public static final String SYS_DATE_TIME_FORMAT = "yyyy-MM-dd HH:mm:ss.SSS";
+	public static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd");
+	public static final SimpleDateFormat DATE_TIME_FORMAT = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 	
 	public static String getCurrentSystemDateTimeStr() {
-		return LocalDateTime.now().toString(SYS_DATE_TIME_FORMAT);
+		return LocalDateTime.now().toString(DATE_TIME_FORMAT.toString());
+	}
+
+	public static java.sql.Date getCurrentSystemDate() {
+		return new java.sql.Date(System.currentTimeMillis());
+	}
+
+	public static java.sql.Timestamp getCurrentSystemTimestamp() {
+		return Timestamp.from(Instant.now());
+	}
+
+	public static java.sql.Date parseDate(String date) {
+	    try {
+	        return new Date(DATE_FORMAT.parse(date).getTime());
+	    } catch (ParseException e) {
+	        throw new IllegalArgumentException(e);
+	    }
+	}
+	 
+	public static java.sql.Timestamp parseTimestamp(String timestamp) {
+	    try {
+	        return new Timestamp(DATE_TIME_FORMAT.parse(timestamp).getTime());
+	    } catch (ParseException e) {
+	        throw new IllegalArgumentException(e);
+	    }
 	}
 }
