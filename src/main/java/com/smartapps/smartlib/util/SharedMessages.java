505f22c47d8831fa2a80fdd43efc0ace2a6c2b92
shylendra <shylendra.tm@gmail.com>
2022-03-14 21:14:58 +1100

    Code cleanup

--- src/main/java/com/smartapps/smartlib/util/SharedMessages.java
+++ src/main/java/com/smartapps/smartlib/util/SharedMessages.java
@@ -2,9 +2,12 @@ package com.smartapps.smartlib.util;
 
 public class SharedMessages {
 
-	/* Log messages */
+	/* API Log messages */
 	public static final String LOG001_PREFIX = "api.log.logprefix";
 	public static final String LOG002_REQUEST = "api.log.request";
 	public static final String LOG003_RESPONSE = "api.log.response";
 	
+	/* API Error messages */
+	public static final String ERR001_RESOURCE_NOTFOUND = "api.error.resource.not.found";
+
 }
